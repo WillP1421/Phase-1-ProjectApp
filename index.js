1a3d32a025fc78c5214f04ae9af6389f7ac85ff6
william <willpatterson5802@gmail.com>
2023-10-16 15:32:37 -0400

    Script

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const animalKingdom = document.getElementById("Mammals")
+const mammalDetail = document.getElementById("mammal-detail")
 
 const displayAnimals = () => {
     fetch("http://localhost:3000/Mammals")
     .then((resp) => resp.json())
     .then((data) => {
         data.forEach((mammal) => {
+            renderMammal(mammal)
         })
     })
 }
+
 
 displayAnimals()
