df19686582a2afcd450d6cc29eb8a70158f4492c
william <willpatterson5802@gmail.com>
2023-10-16 16:44:10 -0400

    event

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ const renderMammal = (mammal) => {
 
     mammalImg.src = mammal.image_url
     mammalImg.alt = "photo of mammal"
+mammalImg.addEventListener("click", () => {
+    const detailImage = document.querySelector(".detail-image")
+    detailImage.src = mammal.image_url;
+
+    const mammalName = document.querySelector('.name')
+    mammalName = mammal.name
+    
+    const mammalFacts = document.querySelector(".fun_fact")
+    mammalFacts = mammal.fun_fact
+})
 
 mammalDiv.appendChild(mammalImg)
 mammalDetail.appendChild(mammalDiv)
